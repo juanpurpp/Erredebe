6ae6bad303c58a3feb8cbf40b6976000a640cfeb
Juan Manuel Sandoval Olavarría <correo.juanm.sandoval@gmail.com>
2024-07-16 00:46:46 -0400

    Details to landing page

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -13,9 +13,9 @@ export default function Home({params}) {
         <div className="p-1 sm:p-0 flex max-w-3xl justify-center items-center flex-col w-full h-full space-y-2">
           <div className="flex flex-col sm:flex-row justify-center items-center sm:space-x-10">
             <Image className="p-1" src="/erredebe.svg" alt="logo" width={200} height={500} />
-            <div className="flex flex-col justify-center items-center max-w-xl space-y-4">
-              <h1 className="w-full text-center sm:text-left text-4xl font-bold">{title}</h1>
-              <h2 className="w-full text-center sm:text-left text-lg font-light">{description}</h2>
+            <div className="flex flex-col justify-center items-start space-y-4">
+              <h1 className="text-center sm:text-left text-4xl font-bold text-slate-800">{title}</h1>
+              <h2 className="text-center sm:text-left text-lg font-light text-slate-800">{description}</h2>
             </div>
           </div>
           <MainLink href={`${params.lang}/app`} label={main_button}></MainLink>
