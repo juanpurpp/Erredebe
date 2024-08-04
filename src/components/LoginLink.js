0f5fb4b2b7b2b0c293690e3c7dbc9b76d3db23ac
Juan Manuel Sandoval Olavarría <correo.juanm.sandoval@gmail.com>
2024-08-04 14:12:03 -0400

    Indifferent origin for redirection on login

--- src/components/LoginLink.js
+++ src/components/LoginLink.js
@@ -1,3 +1,5 @@
+"use client"
+
 import getGoogleAuthUrl from "@/utils/getGoogleAuthUrl"
 import Link from "next/link"
 
--- src/utils/getGoogleAuthUrl.js
+++ src/utils/getGoogleAuthUrl.js
@@ -1,19 +1,22 @@
 const root_url = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_BASE_URL;
 
-const scopes = [
-  'https://www.googleapis.com/auth/userinfo.email', 
-]
-const options ={
-  client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-  redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI,
-  response_type: 'code',
-  scope: scopes.join(''),
-  prompt: 'select_account',
-  access_type: 'offline',
-}
-const qs = new URLSearchParams(options).toString()
+
 
 const getGoogleAuthUrl = () => {
+  const scopes = [
+    'https://www.googleapis.com/auth/userinfo.email', 
+  ]
+
+  const options ={
+    client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    redirect_uri: window.location.origin + process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_SUBPATH,
+    response_type: 'code',
+    scope: scopes.join(''),
+    prompt: 'select_account',
+    access_type: 'offline',
+  }
+  
+  const qs = new URLSearchParams(options).toString()
   return `${root_url}?${qs}`
 }
 
